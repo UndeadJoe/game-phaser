dcf23105ee310b36127435fa8548d396a9c23c54
Maksim Mamaev <maksim.mamaev.12@gmail.com>
2016-09-20 12:57:36 +0300

    from phaser tutorial moving by path

---
+++ scripts/game.js
@@ -0,0 +1,98 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-screen');
+
+var PhaserGame = function () {
+    this.car = null;
+    this.road = null;
+    this.path = [];
+    this.pi = 0;
+
+    this.mode = 1;
+    this.points = {
+        'x': [ 32, 128, 256, 384, 512, 608 ],
+        'y': [ 240, 240, 240, 240, 240, 240 ]
+    };
+};
+
+PhaserGame.prototype = {
+    init: function () {
+        this.stage.backgroundColor = '#204090';
+    },
+
+    preload: function () {
+        this.load.image('car', 'assets/simple_car.svg');
+    },
+
+    create: function () {
+        this.car = game.add.sprite(0, 0, 'car');
+        this.car.scale.set(0.5);
+        this.car.anchor.set(0.5,0.5);
+
+        this.road = this.add.bitmapData(this.game.width, this.game.height);
+        this.road.addToWorld();
+        var py = this.points.y;
+        for (var i = 0; i < py.length; i++)
+        {
+            py[i] = this.rnd.between(32, 432);
+        }
+        console.log(this.points.y);
+
+        this.plot();
+    },
+
+    plot: function () {
+        var x = 1 / (game.width/3);
+        var px, py;
+        var ix = 0;
+
+        this.path = [];
+        this.road.clear();
+
+        for (var i = 0; i <= 1; i += x)
+        {
+            if (this.mode === 0)
+            {
+                px = this.math.linearInterpolation(this.points.x, i);
+                py = this.math.linearInterpolation(this.points.y, i);
+            }
+            else if (this.mode === 1)
+            {
+                px = this.math.bezierInterpolation(this.points.x, i);
+                py = this.math.bezierInterpolation(this.points.y, i);
+            }
+            else if (this.mode === 2)
+            {
+                px = this.math.catmullRomInterpolation(this.points.x, i);
+                py = this.math.catmullRomInterpolation(this.points.y, i);
+            }
+            var node = { x: px, y: py, angle: 0 };
+
+            if (ix > 0)
+            {
+                node.angle = this.math.angleBetweenPoints(this.path[ix - 1], node);
+            }
+
+            this.path.push(node);
+            ix++;
+
+            this.road.rect(px, py, 1, 1, 'rgba(255, 255, 255, 1)');
+        }
+        for (var p = 0; p < this.points.x.length; p++)
+        {
+            this.road.rect(this.points.x[p]-3, this.points.y[p]-3, 6, 6, 'rgba(255, 0, 0, 1)');
+        }
+    },
+
+    update: function () {
+        this.car.x          = this.path[this.pi].x;
+        this.car.y          = this.path[this.pi].y;
+        this.car.rotation   = this.path[this.pi].angle;
+
+        this.pi++;
+        if (this.pi >= this.path.length)
+        {
+            this.pi = 0;
+        }
+    }
+};
+
+game.state.add('Game', PhaserGame, true);
